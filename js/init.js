9c54f5b72db95495e8e5c0198c7c754e47dc2164
Matt Startin <m.j.startin@googlemail.com>
2017-02-02 23:05:57 +0000

    Initial commit of homepage

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	$('.slider').slider();
+	 $('.carousel').carousel();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
